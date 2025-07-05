c33c088c6bf28f6c8598f71b7660316268747e2d
Dawid Jabłoński <xederro@gmail.com>
2025-07-05 11:42:59 +0200

    Added working jira

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/andygrunwald/go-jira"
 	"github.com/apple/pkl-go/pkl"
 	"github.com/xederro/WorklogManager/gen/config"
+	"net/http"
 )
 
 func main() {
@@ -18,5 +20,36 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("Configuration loaded successfully:", cfg)
+	var client *http.Client
+
+	switch cfg.Jira.ServerType {
+	case "cloud":
+		transport := jira.BasicAuthTransport{
+			Username: cfg.Jira.CloudConfig.Email,
+			Password: cfg.Jira.CloudConfig.ApiToken,
+		}
+		client = transport.Client()
+		break
+	case "on-premise":
+		transport := &jira.BearerAuthTransport{
+			Token: cfg.Jira.OnPremiseConfig.Pat,
+		}
+		client = transport.Client()
+		break
+	default:
+		panic("Unknown server type: " + cfg.Jira.ServerType)
+	}
+
+	jiraClient, err := jira.NewClient(client, cfg.Jira.Url)
+	if err != nil {
+		panic(err)
+	}
+
+	u, _, err := jiraClient.User.GetSelfWithContext(context.Background())
+	if err != nil {
+		panic(fmt.Errorf("failed to get user: %w", err))
+	}
+
+	fmt.Printf("Email: %v\n", u.EmailAddress)
+	fmt.Println("Success!")
 }
---
+++ internal/state/Machine.go
@@ -0,0 +1,38 @@
+package state
+
+const (
+	TICKETS = iota
+	WORKLOG
+)
+
+type State struct {
+	state int
+	new   bool
+}
+
+func New() *State {
+	return &State{
+		state: TICKETS,
+		new:   true,
+	}
+}
+
+func (s *State) LogWork() {
+	s.new = true
+	s.state = WORKLOG
+}
+
+func (s *State) Logged() {
+	s.new = false
+	s.state = TICKETS
+}
+
+func (s *State) GetState() int {
+	return s.state
+}
+
+func (s *State) IsNew() bool {
+	n := s.new
+	s.new = false
+	return n
+}
